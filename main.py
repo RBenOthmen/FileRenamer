b99ef9f6cfb888d69d00168af46be56780300ec6
Rami_BenOthmen <ramibenothmen15@gmail.com>
2023-06-28 00:22:34 +0100

    Building the project

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+directory = "C:\\Users\\ramib\\Documents"  # Specify the directory path
+
+# Iterate over all directories and files recursively
+for root, dirs, files in os.walk(directory):
+    for filename in files:
+        file_path = os.path.join(root, filename)
+
+        # Split the filename into words
+        words = filename.split()
+
+        # Extract the last two words
+        new_filename = ' '.join(words[-2:])  # Join the last two words with a space
+
+        # Create the new file path
+        new_file_path = os.path.join(root, new_filename)
+
+        # Rename the file
+        os.rename(file_path, new_file_path)
+        print(f"Renamed: {file_path} --> {new_file_path}")
