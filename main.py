a25fba1280459e0309f82ea8800879b2031f6b61
BenOthmenR <ramibenothmen15@gmail.com>
2023-08-14 20:16:19 +0100

    Added dark theme and updated the GUI

--- main.py
+++ main.py
@@ -16,23 +16,42 @@ def browse_folder():
     folder_selected = filedialog.askdirectory()
     if folder_selected:
         rename_files(folder_selected)
-        status_label.config(text="Files renamed successfully!")
+        status_label.config(text="Files renamed successfully!", fg="green")
 
 # Create the main window
 root = tk.Tk()
 root.title("File Renamer")
 
+# Set a dark theme color scheme
+root.configure(bg="#333333")
+root.option_add("*TButton*Background", "#444444")
+root.option_add("*TButton*Foreground", "white")
+root.option_add("*Label*Background", "#333333")
+root.option_add("*Label*Foreground", "white")
+root.option_add("*TLabel*Background", "#333333")
+root.option_add("*TLabel*Foreground", "white")
+
+# Get the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate the x and y coordinates to center the window
+x = (screen_width - 400) // 2  # Adjusted window width
+y = (screen_height - 250) // 2  # Adjusted window height
+
+root.geometry(f"400x250+{x}+{y}")  # Set window size and position
+
 # Create a label
 label = tk.Label(root, text="Select a folder to rename files:")
-label.pack(pady=10)
+label.pack(pady=20)
 
 # Create a button to browse for a folder
-browse_button = tk.Button(root, text="Browse", command=browse_folder)
+browse_button = tk.Button(root, text="Browse", command=browse_folder, padx=20, pady=10)
 browse_button.pack()
 
 # Create a label to display status
 status_label = tk.Label(root, text="", fg="green")
-status_label.pack(pady=10)
+status_label.pack(pady=20)
 
 # Start the GUI event loop
 root.mainloop()
