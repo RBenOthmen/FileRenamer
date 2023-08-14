fed14e949771a8c856221b4f3584c860585811d1
BenOthmenR <ramibenothmen15@gmail.com>
2023-08-14 18:33:10 +0100

    Added a GUI

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-current_directory = os.getcwd()  # Get the current working directory
+def rename_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            words = filename.split()
+            new_filename = ' '.join(words[-2:])
+            new_file_path = os.path.join(root, new_filename)
+            os.rename(file_path, new_file_path)
+            print(f"Renamed: {file_path} --> {new_file_path}")
 
-# Iterate over all directories and files recursively
-for root, dirs, files in os.walk(current_directory):
-    for filename in files:
-        file_path = os.path.join(root, filename)
+def browse_folder():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected:
+        rename_files(folder_selected)
+        status_label.config(text="Files renamed successfully!")
 
-        # Split the filename into words
-        words = filename.split()
+# Create the main window
+root = tk.Tk()
+root.title("File Renamer")
 
-        # Extract the last two words
-        new_filename = ' '.join(words[-2:])  # Join the last two words with a space
+# Create a label
+label = tk.Label(root, text="Select a folder to rename files:")
+label.pack(pady=10)
 
-        # Create the new file path
-        new_file_path = os.path.join(root, new_filename)
+# Create a button to browse for a folder
+browse_button = tk.Button(root, text="Browse", command=browse_folder)
+browse_button.pack()
 
-        # Rename the file
-        os.rename(file_path, new_file_path)
-        print(f"Renamed: {file_path} --> {new_file_path}")
+# Create a label to display status
+status_label = tk.Label(root, text="", fg="green")
+status_label.pack(pady=10)
 
-input("Press Enter to exit")
+# Start the GUI event loop
+root.mainloop()
